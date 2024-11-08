c16f5088ab5aa943eea66061debf39951400b8d7
JulienLempereurEpitech <julien.lempereur@epitech.eu>
2024-11-08 02:50:55 +0100

    get-horaires ok

--- python/get-horaires.py
+++ python/get-horaires.py
@@ -1,39 +1,55 @@
 from PIL import Image
 import pytesseract
 import re
+from datetime import datetime
 import json
 
 def getDate(text):
   patternDate = r'\d{4}/\d{2}/\d{2}'
-  return re.findall(patternDate, text)
+  return re.findall(patternDate, text)[0]
 
 def getHeure(text):
   patternHeure = r'\d{2}:\d{2}:\d{2}'
-  return re.findall(patternHeure, text)
+  return re.findall(patternHeure, text)[0]
 
 def getCoordonne(text):
   patternE = r'E\s(.*?)\s'
   patternN = r'N\s(.*?)\s'
-  X = re.findall(patternE, text)
-  Y = re.findall(patternN, text)
+  X = re.findall(patternE, text)[0]
+  Y = re.findall(patternN, text)[0]
 
   coordonne = {
-    "X": X[0],
-    "Y": Y[0]
+    "X": X,
+    "Y": Y
   }
   return coordonne
 
-def getHoraires(srcImage):
+def getIsoDate(date, heure):
+
+  day, month, year = map(int, date.split('/'))
+  hour, minute, second = map(int, heure.split(':'))
+
+  return f"{year}-{month:02d}-{day:02d}T{hour:02d}:{minute:02d}:{second:02d}"
+
+def getResult(srcImage):
   date = getDate(srcImage)
   heure = getHeure(srcImage)
-  dateIso = getDateIso
+  dateIso = getIsoDate(date, heure)
+  coordonne = getCoordonne(text)
+  data = {
+    "heure": dateIso,
+    "Y": coordonne['Y'],
+    "X": coordonne['X']
+  }
+  return data
 
-
-
-  date =
-
+################NgonIni
 srcImage = "data/data1.png"
 
 text = pytesseract.image_to_string(Image.open(srcImage))
-result = getHoraires(text)
-print(getHoraires(text))
+# print(text)
+# date = getDate(text)
+# heure = getHeure(text)
+# print(getCoordonne(text))
+# print(getIsoDate(date, heure))
+print(getResult(text))
